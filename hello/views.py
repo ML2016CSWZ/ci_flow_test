248ecc3f95ffbfa8e6e356a5f55b1192061d3123
rdcsung <rdcsung@users.noreply.github.com>
2016-09-29 15:22:00 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
+
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def dl (request):
+    return render (request, 'db.html', 'Machine Learning 2016 CSWZ")
 
 def db(request):
 
