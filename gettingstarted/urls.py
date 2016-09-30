ff429ef849094d80492cbbfa5f20cc76c79a95f1
rdcsung <sungang76@gmail.com>
2016-09-30 14:38:50 +0200

    test form

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     url(r'^db', hello.views.db, name='db'),
     url(r'^dl', hello.views.dl, name='dl'),
     url(r'^learn', hello.views.learn, name='learn'),
+    url(r'^get_name', hello.views.get_name, name='get_name'),
+    url(r'^your_name', hello.views.your_name, name='your_name'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .forms import NameForm
 
 # Create your views here.
 
 def learn(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    return render(request, 'learn.html')
 
 def dl(request):
     # return HttpResponse('Hello from Python!')
@@ -15,7 +16,7 @@ def dl(request):
 
 
 def index (request):
-    return HttpResponse('<pre>' + 'ML2016CSWZ, gang chen say hello, gordon' + '</pre>')
+    return render(request, 'index.html')
 
 def db(request):
 
@@ -25,3 +26,35 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
+
+def your_name(request):
+    # return HttpResponse('Hello from Python!')
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid ():
+            name = form.cleaned_data['your_name']
+            #print form.cleaned_data
+            #print name
+            return HttpResponse (name)
+        else :
+            return HttpResponse ('/post, not valid form/')
+    return HttpResponse('Error')
